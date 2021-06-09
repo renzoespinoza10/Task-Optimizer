35a436bf4674afd6c972a7d997c9202220b1b686
Renzo Espinoza <renzo.espinoza01@gmail.com>
2021-06-09 16:29:37 -0400

    Created canvas

---
+++ test.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import filedialog, Text
+import os
+
+# Set the root of the GUI
+root = tk.Tk()
+root.title("Task Optimizer")
+
+canvas = tk.Canvas(root, height=600, width=500, bg='#47A3BF')
+canvas.pack()
+
+
+root.mainloop()
