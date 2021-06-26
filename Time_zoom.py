eb15a6eaf299549ac49a1e33e089b042bbd1e593
MihirRao01 <70452582+MihirRao01@users.noreply.github.com>
2021-06-26 10:58:11 -0400

    Update Time_zoom.py

--- Time_zoom.py
+++ Time_zoom.py
@@ -1,12 +1,10 @@
-import pandas as pd
-from datetime import datetime
 import time
 import subprocess
+import pandas as pd
+from datetime import datetime
 
-# read the excel file to get meeting details
-df = pd.read_excel('meetingschedule.xlsx')
-df_new = pd.DataFrame()
-
+# reading the meeting details
+df = pd.read_csv('meetingschedule.csv')
 # Check the current system time
 ct = datetime.now().strftime("%H:%M")
 dt = datetime.now().strftime("%m/%d/%Y")
@@ -14,11 +12,14 @@ print(ct)
 print(dt)
 # Check if the current time is mentioned in the Dataframe
 while(True):
+    ct = datetime.now().strftime("%H:%M")
+    dt = datetime.now().strftime("%m/%d/%Y")
     if dt in df.Date.values:
-        print("Date is read correctly from file")
+        #print("Date is read correctly")
         if ct in df.Time.values:
             df_new = df[df['Time'].astype(str).str.contains(ct)]
-            print("opening zoom")
-            # Open the Zoom app
-            subprocess.Popen("C:\\Users\\rezog\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe")
-            time.sleep(20)
+            #print("Time is read correctly")
+            print(df_new.iloc[0, 2])
+        # Open the Zoom app
+        #   subprocess.Popen("C:\\Users\\rezog\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe")
+            # time.sleep(60)
